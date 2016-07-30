994b2a9c44ab9bee96e605d86aae69da494371b9
David Buchan-Swanson <david.buchanswanson@gmail.com>
2016-07-30 20:32:49 +1000

    spec out the files

---
+++ app/client/components/Uploader.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export class Uploader extends Component {
+  constructor(){
+
+  }
+
+  doUpload(){
+
+  }
+
+  render(){
+    return (
+      <div className="display-1">Hello, World!</div>
+    )
+  }
+}
---
+++ app/client/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Uploader } from './components/Uploader';
+
+const mountNode = document.getElementById('app');
+
+ReactDOM.render(<Uploader />, mountNode);
