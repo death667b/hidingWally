21ac3faf78109be4c0b624b4eb19180d720ed825
David Buchan-Swanson <david.buchanswanson@gmail.com>
2016-07-31 09:15:26 +1000

    make the Toast component a little (a lot) smarter

--- app/client/components/Toast.jsx
+++ app/client/components/Toast.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const Toast = props => {
-  const rawType = props.type.split('-')[1];
-  const type = `${rawType.substr(0,1).toUpperCase()}${rawType.substr(1)}`;
-  return (
-    <div className={`alert ${props.type}`}><strong>{type}!</strong> {props.message}</div>
-  )
-};
+export class Toast extends Component {
+
+  constructor(props) {
+    super(props);
+    this.message = this.props.message;
+    this.alertType = this.props.type;
+    this.type = `${rawType.substr(0,1).toUpperCase()}${rawType.substr(1)}`;
+    this.timeOut = this.props.timeOut || 2000;
+  }
+
+  componentWillMount() {
+    this.timer = setTimeout(() => this.props.clearToast(), this.timeOut);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  render() {
+    return (
+      <div className={`alert ${this.alertType}`}><strong>{this.type}!</strong> {this.message}</div>
+    )
+  }
+
+}
