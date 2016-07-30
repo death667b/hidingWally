0326a1ba096771429098be4a54946198f16693c0
Ian Daniel <iw.daniel@student.qut.edu.au>
2016-07-30 22:21:13 +1000

    i dont know why

--- app/iantest.js
+++ app/iantest.js
@@ -1,3 +1,3 @@
-import ParseEmail from './lib/types/ParseEmail'; 
+import ParseEmail from '../lib/types/ParseEmail'; 
 
 console.log(ParseEmail.filter()['Obscure entire email']("name@domain.com.au"));
