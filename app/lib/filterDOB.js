1f3f5b9105ebc1f0b930cc01621572feefed0d50
Ian Daniel <iw.daniel@student.qut.edu.au>
2016-07-30 14:15:01 +1000

    getMonth and getYear working

--- app/lib/filterDOB.js
+++ app/lib/filterDOB.js
@@ -1,68 +1,81 @@
-/**
- * Test if supplied string is in a valid date format
- *	@param {String} date to test
- *	@return {Boolean} True if valid date
- */
-function isDate(date){
-	if (Date.parse(date)) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-/**
- * Filters a DOB based on a year range and optionally month range
- * @param {String} dob
- * @param {Number} yearRange
- * @param {Number} monthRange - If no month givern, defaults to 1
- * @return {String} Filtered DOB
- */
-function filterDOB(dob, yearRange, monthRange){
-	yearRange = (yearRange < 0) ? 1 : yearRange;
-
-	monthRange = (typeof monthRange === 'undefined') ? 0 : monthRange;
-	monthRange = (monthRange > 12) ? 12 : monthRange;
-	monthRange = (monthRange < 0) ? 0 : monthRange;
-
-	if (monthRange > 0) {
-		var month = getMonth(dob);
-		var minMonth = month - (month % monthRange);
-		minMonth = (minMonth < 1) ? 1 : minMonth;
-		var maxMonth = minMonth + (monthRange-1);
-	} else {
-		var month = "";
-		var minMonth = "";
-		var maxMonth = "";
+class filterDate {
+	/**
+	 * Test if supplied string is in a valid date format
+	 *	@param {String} date to test
+	 *	@return {Boolean} True if valid date
+	 */
+	static isDate(date){
+		if (Date.parse(date)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	var year = getYear(dob);
-	var minYear = year - (year % yearRange);
-	var maxYear = minYear + (yearRange-1);
+	/**
+	 * Filters a DOB based on a year range and optionally month range
+	 * @param {String} dob
+	 * @param {Number} yearRange
+	 * @param {Number} monthRange - If no month givern, defaults to 1
+	 * @return {String} Filtered DOB
+	 */
+	static DOB(dob, yearRange, monthRange){
+		yearRange = (yearRange < 0) ? 1 : yearRange;
+
+		monthRange = (typeof monthRange === 'undefined') ? 0 : monthRange;
+		monthRange = (monthRange > 12) ? 12 : monthRange;
+		monthRange = (monthRange < 0) ? 0 : monthRange;
+
+		if (monthRange > 0) {
+			var month = getMonth(dob);
+			var minMonth = month - (month % monthRange);
+			minMonth = (minMonth < 1) ? 1 : minMonth;
+			var maxMonth = minMonth + (monthRange-1);
+		} else {
+			var month = "";
+			var minMonth = "";
+			var maxMonth = "";
+		}
+
+		var year = getYear(dob);
+		var minYear = year - (year % yearRange);
+		var maxYear = minYear + (yearRange-1);
 
 
 
 
 
-	//console.log("Year range: " + minYear + " >= dobYear <= " + maxYear);
-	//console.log("Month range: " + minMonth + " >= dobMonth <= " + maxMonth);
+		//console.log("Year range: " + minYear + " >= dobYear <= " + maxYear);
+		//console.log("Month range: " + minMonth + " >= dobMonth <= " + maxMonth);
 
-	return "return string";
-}
+		return "return string";
+	}
 
-function getMonth(dobString){
-	var dob = new Date(dobString);
-	return dob.getMonth()+1;
-}
+	/**
+	 * Returns the month number
+	 * @param {String} Date as a valid string
+	 * @return {Number} The month number
+	 */
+	static getMonth(dobString){
+		var dob = new Date(dobString);
+		return dob.getMonth()+1;
+	}
 
-function getYear(dobString){
-	var dob = new Date(dobString);
-	return dob.getFullYear();
+	/**
+	 * Returns the full year. eg 1980
+	 * @param {String} Date as a valid string
+	 * @return {Number} 4 digit year
+	 */
+	static getYear(dobString){
+		var dob = new Date(dobString);
+		return dob.getFullYear();
+	}
 }
 
 //var dob1 = "25-dec-00";
 
 //var yearTRange = 5;
 //var monthTRange = 5;
+console.log(filterDate.getYear("25 dec 1980"));
 
 //console.log(filterDOB(dob1, yearTRange, 5));
