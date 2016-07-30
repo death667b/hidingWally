5985ccab5a60b17e436dcf90b76f9b7ba028f27e
David Buchan-Swanson <david.buchanswanson@gmail.com>
2016-07-30 22:41:19 +1000

    toasts now display correctly!

--- app/client/components/Uploader.jsx
+++ app/client/components/Uploader.jsx
@@ -9,14 +9,23 @@ export class Uploader extends Component {
 
   doUpload(event) {
     event.preventDefault();
-    return this.props.showToast('err', 'No File Selected');
     // gonna have to do 'new FormData()'
-    // const form = new FormData();
-    // console.log(event.target.files);
-    // if (event.target.files.length < 1){
-    //   return this.props.showToast('err', 'No File Selected');
-    // }
-    // form.append('file', event.target.files[0]);
+    const form = new FormData();
+    console.log(event.target.files);
+    if (event.target.files.length < 1) {
+      return this.props.showToast('alert-warning', 'No File Selected');
+    }
+    form.append('file', event.target.files[0]);
+    fetch('/upload', {
+      method: 'POST',
+      body: form,
+    })
+      .then(res => {
+        if (!res.ok) {
+          return this.props.showToast('alert-warning', 'File has failed to upload.');
+        }
+        return this.props.showToast('alert-success', 'File has been uploaded!');
+      })
   }
 
   render() {
