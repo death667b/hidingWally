2fd5d28d475bbf95c9ab9d67bf2dfe09077fc5e1
Ian Daniel <iw.daniel@student.qut.edu.au>
2016-07-31 14:58:43 +1000

    the parse for latlong has been updated to...
    be able to accept (number, number) format

--- app/test/iantest.js
+++ app/test/iantest.js
@@ -1,4 +1,5 @@
 import LatLong from '../lib/types/LatLong'; 
 
-console.log(LatLong.filters()["Round to Nearest 100m"]("-68.17231"));
+console.log(LatLong.filters()["Round to Nearest 100Km"]("-68.17231, 178.40749"));
+console.log(LatLong.filters()["Round to Nearest 100Km"]("-68.17231"));
 
