7f363e7325d5eccf93eb83047d45503c2d5cd7ca
Ian Daniel <iw.daniel@student.qut.edu.au>
2016-07-31 11:38:48 +1000

    You rock David!

--- app/test/iantest.js
+++ app/test/iantest.js
@@ -1,4 +1,4 @@
 import ParseName from '../lib/types/ParseName'; 
 
 //console.log(ParseName.filters()['Round to nearest 20 years']('11'));
-console.log(ParseName.isValid("ian daniel"));
+console.log(ParseName.isValid("daniel, ian"));
