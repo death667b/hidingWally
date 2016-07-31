6b148b86b83e19aa803d4b6d773f2529276482a4
Ian Daniel <iw.daniel@student.qut.edu.au>
2016-07-31 15:24:10 +1000

    ive changed upload.jsx - looks no diff

--- app/client/components/Upload.jsx
+++ app/client/components/Upload.jsx
@@ -3,25 +3,27 @@ import React from 'react';
 import { Uploader } from './Uploader';
 
 export const Upload = props => (
-  <div className="upload-activity">
-    <div className='jumbotron'>
-      <div className='row'>
-        <div className='col-xs-12'><h1 className='display-3'>DAAASGÜD</h1></div>
-        <div className='col-xs-12'>
-          <p className='lead text center'>
-            is a toolkit for data anonymisation. It lets you choose what
-            you want to anonymise and to what granularity.
-          </p>
+  <div className="container">
+    <div className="upload-activity">
+      <div className="row">
+        <div className='jumbotron'>
+          <div className='col-xs-12'><h1 className='display-3'>DAAASGÜD</h1></div>
+          <div className='col-xs-12'>
+            <p className='lead text center'>
+              is a toolkit for data anonymisation. It lets you choose what
+              you want to anonymise and to what granularity.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="row">
-        <div className="col-xl-5">
-          <img src="/favi/binary2.jpg" alt="Anon" />
-        </div>
-        <div className="col-xl-7">
-          <Uploader {...props} />
-        </div>
+      <div className="row">
+          <div className="col-xl-5">
+            <img src="/favi/binary2.jpg" alt="Anon" />
+          </div>
+          <div className="col-xl-7">
+            <Uploader {...props} />
+          </div>
+      </div>
     </div>
   </div>
 );
